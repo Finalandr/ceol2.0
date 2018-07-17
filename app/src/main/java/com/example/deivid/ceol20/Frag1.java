6f7b1d2c36fce8d9d6637ad0229d4ac356d3c675
finalandr <20163524@itla.edu.do>
2018-07-17 19:49:28 -0400

    No toquen aun

--- app/src/main/java/com/example/deivid/ceol20/Frag1.java
+++ app/src/main/java/com/example/deivid/ceol20/Frag1.java
@@ -1,5 +1,6 @@
 package com.example.deivid.ceol20;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,10 +8,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Frag1 extends Fragment {
 
+    Button play;
+    MediaPlayer mp;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag1_layout, container, false);
+        View viewmusic= inflater.inflate(R.layout.frag1_layout, container, false);
+
+        play=viewmusic.findViewById(R.id.buttonplay);
+
+
+
+        return viewmusic;
     }
 }
