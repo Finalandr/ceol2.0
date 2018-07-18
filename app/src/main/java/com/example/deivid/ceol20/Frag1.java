a04761b0e93ce4dcfd290a9be3c99968ab3be8c4
finalandr <20163524@itla.edu.do>
2018-07-17 20:28:40 -0400

    No toquen aun

--- app/src/main/java/com/example/deivid/ceol20/Frag1.java
+++ app/src/main/java/com/example/deivid/ceol20/Frag1.java
@@ -9,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import static com.example.deivid.ceol20.R.raw.pista;
 
 public class Frag1 extends Fragment {
 
@@ -16,13 +19,26 @@ public class Frag1 extends Fragment {
     MediaPlayer mp;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View viewmusic= inflater.inflate(R.layout.frag1_layout, container, false);
+        final View viewmusic= inflater.inflate(R.layout.frag1_layout, container, false);
 
         play=viewmusic.findViewById(R.id.buttonplay);
 
+        mp=MediaPlayer.create(getActivity(),R.raw.pista);
 
-
-
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mp.isPlaying()){
+                    mp.pause();
+                    play.setBackgroundResource(R.drawable.play2);
+                    Toast.makeText(getActivity(),"Pausa",Toast.LENGTH_SHORT).show();
+                }else{
+                    mp.start();
+                    play.setBackgroundResource(R.drawable.pause2);
+                    Toast.makeText(getActivity(),"Play",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         return viewmusic;
     }
 }
